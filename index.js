26602c10066ff9a1d7b9f54b6641cd5cf536a4f0
Kelvin <kierulfkelvin@gmail.com>
2025-05-31 21:17:01 +0800

    Objects ++ Dynamic Programming

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
- console.log("Putanginang yan");
+ console.log("Hello");
  let x;
  console.log(x);
- x = "nigga";
- console.log("Putanginang " + x);
+ x = "World";
+ console.log("Hellow " + x);
+ 
+
+ //typeoff
+ //Ctrl + L = clear console
+
+ let person = {
+    name: 'Kelvin',
+    age: 20
+ };
+
+ console.log(person);
+ // Dot Notation
+ console.log(person.age + " " + person.name );
+ // Bracket Notation - has to be "name" not [name] only
+ person["name"] = "Joshua";
+ //Dot Notation
+ person.age = 32;
+ 
+ console.log(person.age + " " + person.name );
